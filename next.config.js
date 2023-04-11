45490239c588c7566f991193cfa39ab4dafd5e87
Žane Suhadolnik <zanesuhi@gmail.com>
2023-04-11 09:11:34 +0200

    Updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const withFonts = require('next-fonts');
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
 };
 
 module.exports = withFonts(nextConfig);
